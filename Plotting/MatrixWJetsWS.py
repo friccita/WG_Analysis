943cbecc2dbc227228d58e95e8636f90bf2b8d2d
Francesca Ricci-Tam <francesca.ricci-tam@cern.ch>
2019-11-27 03:37:32 -0500

    Matrix calculation

--- Plotting/MatrixWJetsWS.py
+++ Plotting/MatrixWJetsWS.py
@@ -133,6 +133,7 @@ def make_wjets_matrix( sampMan, sample, sel_base, eta_cut, isdata=False, suffix=
     ph_sel_preid = ' ph_passEleVeto[0] && ph_passHOverEMedium[0] && ph_passNeuIsoCorrMedium[0] && ph_passPhoIsoCorrMedium[0]'
     ph_sel_chiso_incl = 'ph_passSIEIEMedium[0]'
     ph_sel_sieie_incl = 'ph_passChIsoCorrMedium[0]'
+    chiso_fail_sel = 'ph_chIsoCorr[0] > 4 && ph_chIsoCorr[0] < 10'
 
     #deltaR_real_sel         = 'min(dr_lep_ph, dr_lep2_ph) > 0.1 && min(dr_lep_ph, dr_lep2_ph) < 0.8'
     deltaR_real_sel         = 'min(dr_lep_ph, dr_lep2_ph) > 0.4'
@@ -150,25 +151,23 @@ def make_wjets_matrix( sampMan, sample, sel_base, eta_cut, isdata=False, suffix=
         myweight = '(NLOWeight*PUWeight*mu_trigSF*mu_idSF*mu_isoSF*ph_idSF*ph_csevSF)'
 
 
-    #real_sel_sieie_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, ph_sel_sieie_incl, deltaR_real_sel] ) #set pt cut here
     real_sel_sieie_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_40Up, ph_sel_preid, ph_sel_sieie_incl] ) #set pt cut here
     real_sel_sieie_incl = '(' + real_sel_sieie_incl + ')*' + myweight
 
-    #real_sel_chiso_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, ph_sel_chiso_incl, deltaR_real_sel] ) #set pt cut here
     real_sel_chiso_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_40Up, ph_sel_preid, ph_sel_chiso_incl] ) #set pt cut here
     real_sel_chiso_incl = '(' + real_sel_chiso_incl + ')*' + myweight
 
-    fake_sel_sieie_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, ph_sel_sieie_incl, deltaR_fake_sel] ) #set pt cut here
+    fake_sel_sieie_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, chiso_fail_sel] ) #set pt cut here
     fake_sel_sieie_incl = '(' + fake_sel_sieie_incl + ')*' + myweight
 
-    fake_sel_chiso_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, ph_sel_chiso_incl, deltaR_fake_sel] ) #set pt cut here
+    fake_sel_chiso_incl = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid, chiso_fail_sel] ) #set pt cut here
     fake_sel_chiso_incl = '(' + fake_sel_chiso_incl + ')*' + myweight
 
 
     predR_sel = ' && '.join( [sel_base, ph_sel_basic, ph_pt_15To25, ph_sel_preid] ) #set pt cut here
     predR_sel = '(' + predR_sel + ')*' + myweight
 
-    zpeak_sel = ' && '.join( [sel_base, ph_sel_basic, ph_pt_40Up] ) 
+    zpeak_sel = ' && '.join( [sel_base, ph_sel_basic, ph_pt_40Up, ph_sel_preid, ph_sel_sieie_incl, ph_sel_chiso_incl] ) 
     zpeak_sel =  '(' + zpeak_sel + ')*' + myweight
 
     sel_smp_zpeak = 'mu_n==2 && m_ll < 106.2 && m_ll > 76.2 && mu_pt_rc[0] > 25. && mu_pt_rc[1] > 25. && mu_hasTrigMatch[0] && mu_passTight[0] && mu_hasTrigMatch[1] && mu_passTight[1] && (mu_charge[0]*mu_charge[1] < 0) && ph_n == 0'
@@ -194,11 +193,13 @@ def make_wjets_matrix( sampMan, sample, sel_base, eta_cut, isdata=False, suffix=
     binning_mt = (100,0.,1000.)
     binning_dr = (40, 0., 4.)
     binning_vtx = (40, 0., 40.)
+    binning_njet = (10,-0.5, 9.5)
 
     chIso_var = 'ph_chIsoCorr[0]'
     sigIEIE_var = 'ph_sigmaIEIEFull5x5[0]'
     mll_var = 'm_ll'
     dr_var = 'min(dr_lep_ph,dr_lep2_ph)'
+    jetn_var = 'jet_n'
     vtx_var = 'vtx_n'
     pu_var = 'pu_n'
     mt_var = 'mt_res'
@@ -208,19 +209,21 @@ def make_wjets_matrix( sampMan, sample, sel_base, eta_cut, isdata=False, suffix=
     hist_real_sigmaIEIE = clone_sample_and_draw( sampMan, sample, sigIEIE_var, real_sel_sieie_incl, binning_sigIEIE )
     hist_real_chIso = clone_sample_and_draw( sampMan, sample, chIso_var, real_sel_chiso_incl, binning_chIso )
 
-    #hist_real_sigmaIEIE_FR = clone_sample_and_draw( sampMan, sample, sigIEIE_var, real_sel_sieie_incl, binning_sigIEIE_FR )
+    hist_real_sigmaIEIE_FR = clone_sample_and_draw( sampMan, sample, sigIEIE_var, real_sel_sieie_incl, binning_sigIEIE_FR )
     #hist_real_chIso_FR = clone_sample_and_draw( sampMan, sample, chIso_var, real_sel_chiso_incl, binning_chIso_FR )
 
-    #hist_fake_sigmaIEIE = clone_sample_and_draw( sampMan, sample, sigIEIE_var, fake_sel_sieie_incl, binning_sigIEIE )
-    #hist_fake_chIso = clone_sample_and_draw( sampMan, sample, chIso_var, fake_sel_chiso_incl, binning_chIso )
+    hist_fake_sigmaIEIE = clone_sample_and_draw( sampMan, sample, sigIEIE_var, fake_sel_sieie_incl, binning_sigIEIE )
+    hist_fake_chIso = clone_sample_and_draw( sampMan, sample, chIso_var, fake_sel_chiso_incl, binning_chIso )
 
-    #hist_fake_sigmaIEIE_FR = clone_sample_and_draw( sampMan, sample, sigIEIE_var, fake_sel_sieie_incl, binning_sigIEIE_FR )
+    hist_fake_sigmaIEIE_FR = clone_sample_and_draw( sampMan, sample, sigIEIE_var, fake_sel_sieie_incl, binning_sigIEIE_FR )
     #hist_fake_chIso_FR = clone_sample_and_draw( sampMan, sample, chIso_var, fake_sel_chiso_incl, binning_chIso_FR )
 
     hist_mll = clone_sample_and_draw( sampMan, sample, mll_var, zpeak_sel, binning_mll )
     #hist_mll_smp = clone_sample_and_draw( sampMan, sample, mll_var, sel_smp_zpeak, binning_mll )
     hist_dr_nosieie = clone_sample_and_draw( sampMan, sample, dr_var, real_sel_sieie_incl, binning_dr )
     hist_dr_nochiso = clone_sample_and_draw( sampMan, sample, dr_var, real_sel_chiso_incl, binning_dr )
+    hist_jetn_nosieie = clone_sample_and_draw( sampMan, sample, jetn_var, real_sel_sieie_incl, binning_njet )
+    hist_jetn_nochiso = clone_sample_and_draw( sampMan, sample, jetn_var, real_sel_chiso_incl, binning_njet )
     hist_nvtx = clone_sample_and_draw( sampMan, sample, vtx_var, zpeak_sel, binning_vtx )
     hist_mt = clone_sample_and_draw( sampMan, sample, mt_var, zpeak_sel, binning_mt )
     hist_met = clone_sample_and_draw( sampMan, sample, met_var, zpeak_sel, binning_mt )
@@ -228,18 +231,20 @@ def make_wjets_matrix( sampMan, sample, sel_base, eta_cut, isdata=False, suffix=
     
     sampMan.outputs['%s_sigmaIEIE_real_%s' %(sample,suffix)] = hist_real_sigmaIEIE
     sampMan.outputs['%s_chIso_real_%s' %(sample,suffix)] = hist_real_chIso
-    #sampMan.outputs['%s_sigmaIEIE_real_FR_%s' %(sample,suffix)] = hist_real_sigmaIEIE_FR
+    sampMan.outputs['%s_sigmaIEIE_real_FR_%s' %(sample,suffix)] = hist_real_sigmaIEIE_FR
     #sampMan.outputs['%s_chIso_real_FR_%s' %(sample,suffix)] = hist_real_chIso_FR
 
-    #sampMan.outputs['%s_sigmaIEIE_fake_%s' %(sample,suffix)] = hist_fake_sigmaIEIE
-    #sampMan.outputs['%s_chIso_fake_%s' %(sample,suffix)] = hist_fake_chIso
-    #sampMan.outputs['%s_sigmaIEIE_fake_FR_%s' %(sample,suffix)] = hist_fake_sigmaIEIE_FR
+    sampMan.outputs['%s_sigmaIEIE_fake_%s' %(sample,suffix)] = hist_fake_sigmaIEIE
+    sampMan.outputs['%s_chIso_fake_%s' %(sample,suffix)] = hist_fake_chIso
+    sampMan.outputs['%s_sigmaIEIE_fake_FR_%s' %(sample,suffix)] = hist_fake_sigmaIEIE_FR
     #sampMan.outputs['%s_chIso_fake_FR_%s' %(sample,suffix)] = hist_fake_chIso_FR
 
     sampMan.outputs['%s_mll_%s' %(sample,suffix)] = hist_mll
     #sampMan.outputs['%s_mllsmp_%s' %(sample,suffix)] = hist_mll_smp
-    sampMan.outputs['%s_dr_nosieieie_%s' %(sample,suffix)] = hist_dr_nosieie
+    sampMan.outputs['%s_dr_nosieie_%s' %(sample,suffix)] = hist_dr_nosieie
     sampMan.outputs['%s_dr_nochiso_%s' %(sample,suffix)] = hist_dr_nochiso
+    sampMan.outputs['%s_jetn_nosieie_%s' %(sample,suffix)] = hist_jetn_nosieie
+    sampMan.outputs['%s_jetn_nochiso_%s' %(sample,suffix)] = hist_jetn_nochiso
     sampMan.outputs['%s_vtxn_%s' %(sample,suffix)] = hist_nvtx
     sampMan.outputs['%s_mtres_%s' %(sample,suffix)] = hist_mt
     sampMan.outputs['%s_pun_%s' %(sample,suffix)] = hist_npu
